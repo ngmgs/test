7274dda5302faaa0f23ce7bce9301138b0915ec1
ngmgs <112070929+ngmgs@users.noreply.github.com>
2022-08-25 22:22:13 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -2,7 +2,7 @@ from discord.ext import commands
 from os import getenv
 import traceback
 
-bot = commands.Bot(command_prefix='/')
+bot = commands.Bot(command_prefix='/',intents=discord.Intents.all())
 
 
 @bot.event
