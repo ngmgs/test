3368153f5300555c169a0d1b04e3837ecdde809f
ngmgs <112070929+ngmgs@users.noreply.github.com>
2022-08-27 00:55:42 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -13,7 +13,7 @@ async def on_ready():
     norolemember = [i for i in guild.members]
     print("on_ready")
     print(discord.__version__)
-    print(norolemember[0].members)
+    print(norolemember[0].member)
 
 
 @bot.event
