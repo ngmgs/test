7aba46dcfc8e393ddd49ceabbdbf723df9bf3091
ngmgs <112070929+ngmgs@users.noreply.github.com>
2022-08-26 19:34:41 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -15,8 +15,13 @@ async def on_command_error(ctx, error):
 
 
 @bot.command()
-async def neko(ctx):
-    await ctx.send('にゃー')
+@has_permissions(manage_roles=True, kick_members=True)
+async def kick(ctx, member:discord.Member, reason):
+   await member.kick(reason=reason)
+   embed=discord.Embed(title="KICK", color=0xff0000)
+   embed.add_field(name="メンバー", value=f"{member.mention}", inline=False)
+   embed.add_field(name="理由", value=f"{reason}", inline=False)
+   await ctx.send(embed=embed)
 
 
 token = getenv('DISCORD_BOT_TOKEN')
