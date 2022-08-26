5dea395f21965e7777c8e8b8006c36fad84f0754
ngmgs <112070929+ngmgs@users.noreply.github.com>
2022-08-26 20:13:38 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -16,10 +16,11 @@ async def on_command_error(ctx, error):
 
 @bot.command()
 @commands.has_permissions(administrator=True)
-async def kick(ctx, member:discord.Member):
+async def kick(ctx, member:discord.Member, reason):
    await member.kick(reason=reason)
    embed=discord.Embed(title="KICK", color=0xff0000)
    embed.add_field(name="メンバー", value=f"{member.mention}", inline=False)
+   embed.add_field(name="理由", value=f"{reason}", inline=False)
    await ctx.send(embed=embed)
 
 
