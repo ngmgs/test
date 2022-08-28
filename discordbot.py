508931ebed3561b806beea9f4ca0044556aa42da
ngmgs <112070929+ngmgs@users.noreply.github.com>
2022-08-28 20:28:53 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -96,7 +96,7 @@ channel_sent = None
 async def send_message_every_10sec():         
     guild = bot.guilds[0]
     now = datetime.now().strftime('%A/%H:%M')
-    if now == 'Sunday/11:27':
+    if now == 'Sunday/11:29':
         await channel_sent.send(now + "全員のkagi権限削除")        
         role = discord.utils.get(guild.roles, name = "kagi")
         norolemember = [i for i in guild.members]
@@ -109,13 +109,13 @@ async def send_message_every_10sec():
         await channel_sent.send(now + "鍵部屋をプライベート解除")                      
         channel_sent2 = bot.get_channel(1012928069402636390)
         role2 = discord.utils.get(guild.roles, name = "@everyone")
-        await channel_sent2.set_permissions(role2, read_messages=true)
+        await channel_sent2.set_permissions(role2, read_messages=True)
 
     if now == 'Sunday/11:21': 
         await channel_sent.send(now + "鍵部屋をプライベート解除")
         channel_sent2 = bot.get_channel(1012928069402636390)
         role2 = discord.utils.get(guild.roles, name = "@everyone")
-        await channel_sent2.set_permissions(role2, read_messages=true)
+        await channel_sent2.set_permissions(role2, read_messages=True)
            
     if now == 'Sunday/11:21': 
         await channel_sent.send(now + "鍵部屋をプライベート化")
