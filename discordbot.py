b7f62ac1343ec9fc863544aadaff882cbac080db
ngmgs <112070929+ngmgs@users.noreply.github.com>
2022-09-03 18:41:00 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -12,28 +12,16 @@ channel_sent = None
 
 
 
-"""
-@bot.event
-async def on_ready():
-    global channel_sent 
-    channel_sent = bot.get_channel(1012237139729199136)
-    send_message_every_10sec.start() #定期実行するメソッドの後ろに.start()をつける    
-"""
-
-@tasks.loop(minutes=1)
-async def send_message_every_10sec():         
-    guild = bot.guilds[0]
-    t_delta = timedelta(hours=9)
-    JST = timezone(t_delta, 'JST')
+@tasks.loop(seconds=10)
+async def loop():
     now = datetime.now(JST).strftime('%A/%H:%M')
     channel_sent = bot.get_channel(1012237139729199136)
     await channel_sent.send(now)    
+    
+loop.start()
 
 
 
 
-
-
-send_message_every_10sec.start()
 token = getenv('DISCORD_BOT_TOKEN')
 bot.run(token)
