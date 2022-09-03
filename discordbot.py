99125d04001151765b59ef3abafad1d3f0bab11d
ngmgs <112070929+ngmgs@users.noreply.github.com>
2022-09-03 19:02:04 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -2,8 +2,6 @@ import discord
 import traceback
 from discord.ext import commands
 from discord.ext import tasks
-from os import getenv
-from datetime import datetime, timezone, timedelta, time
 
 
 bot = commands.Bot(command_prefix="!",intents=discord.Intents.all())
