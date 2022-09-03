10d322615f1121746107d1afb036ed44b5f1783b
ngmgs <112070929+ngmgs@users.noreply.github.com>
2022-09-03 19:38:48 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -5,9 +5,6 @@ from discord.ext import tasks
 from os import getenv
 
 bot = commands.Bot(command_prefix="!",intents=discord.Intents.all())
-channel_sent = None
-
-
 
 
 @tasks.loop(seconds=10)
@@ -15,9 +12,6 @@ async def loop_10sec():
     print("aaa")  
     
 loop_10sec.start()
-
-
-
 
 
 token = getenv('DISCORD_BOT_TOKEN')
