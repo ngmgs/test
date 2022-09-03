cc894900a8f9c089862674e1b5afad364ff6c406
ngmgs <112070929+ngmgs@users.noreply.github.com>
2022-09-03 22:22:20 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,5 +1,6 @@
 import discord
 import traceback
+import asyncio
 from discord.ext import commands
 from discord.ext import tasks
 from os import getenv
@@ -7,12 +8,16 @@ from os import getenv
 bot = commands.Bot(command_prefix="!",intents=discord.Intents.all())
 
 
+
 @tasks.loop(seconds=10)
 async def loop_10sec():
     print("aaa")  
-    
-loop_10sec.start()
 
+async def fn():
+    await loop_10sec.start()
+
+loop_ = asyncio.get_event_loop()
+loop_.run_until_complete(fn())
 
 token = getenv('DISCORD_BOT_TOKEN')
 bot.run(token)
