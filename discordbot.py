f2ae83454c9702ce2d3a127d77453f15ec026fe8
ngmgs <112070929+ngmgs@users.noreply.github.com>
2022-09-05 01:17:13 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -149,11 +149,11 @@ async def ping(ctx):
 @bot.command()
 @commands.has_permissions(administrator=True)
 async def kick(ctx, member:discord.Member, reason):
-   await member.kick(reason=reason)
-   embed=discord.Embed(title="KICK", color=0xff0000)
-   embed.add_field(name="メンバー", value=f"{member.mention}", inline=False)
-   embed.add_field(name="理由", value=f"{reason}", inline=False)
-   await ctx.send(embed=embed)
+    await member.kick(reason=reason)
+    embed=discord.Embed(title="KICK", color=0xff0000)
+    embed.add_field(name="メンバー", value=f"{member.mention}", inline=False)
+    embed.add_field(name="理由", value=f"{reason}", inline=False)
+    await ctx.send(embed=embed)
 
 
 token = getenv('DISCORD_BOT_TOKEN')
