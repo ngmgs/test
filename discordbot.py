793b750b5a7abab7803fd06753752bc8c3e84f59
ngmgs <112070929+ngmgs@users.noreply.github.com>
2022-09-09 01:11:53 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -9,67 +9,8 @@ from datetime import datetime, timezone, timedelta, time
 
 bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
 is_text = {}
+channel_sent = None
 
-@bot.command()
-async def test(ctx):
-    guild = bot.guilds[0]
-    norolemember = [i for i in guild.members]
-    norolemember2 = [i for i in guild.members if len(i.roles) == 1]
-
-    for item in norolemember:
-        await ctx.send(item)
-
-    await ctx.send(discord.__version__)
-
-    for item in norolemember2:
-        await ctx.send(item)
-        await item.kick()
-
-@bot.command()
-async def tes(ctx):
-    guild = bot.guilds[0]
-    norolemember = [i for i in guild.members]
-    norolemember2 = [i for i in guild.members if len(i.roles) == 2]
-
-    for item in norolemember:
-        await ctx.send(item)
-
-    await ctx.send(discord.__version__)
-
-    for item in norolemember2:
-        await ctx.send(item)
-
-@bot.command()
-async def norole(ctx):  # guildオブジェクトを渡してください
-    guild = bot.guilds[0]
-    role = discord.utils.get(guild.roles, name="kagi")
-    norolemember = [i for i in guild.members]
-    for i in norolemember:
-        try:
-            await ctx.send(i)
-            await i.remove_roles(role, atomic=True)
-        except discord.Forbidden:
-            print("権限が足りません")
-
-@bot.command()
-async def everyone(ctx):
-    guild = bot.guilds[0]
-    channel_sent2 = bot.get_channel(1012928069402636390)
-    role = discord.utils.get(guild.roles, name="@everyone")
-    await channel_sent2.set_permissions(role, read_messages=False)
-
-@bot.command()
-async def ping(ctx):
-    await ctx.send(discord.__version__)
-
-@bot.command()
-@commands.has_permissions(administrator=True)
-async def kick(ctx, member: discord.Member, reason):
-    await member.kick(reason=reason)
-    embed = discord.Embed(title="KICK", color=0xff0000)
-    embed.add_field(name="メンバー", value=f"{member.mention}", inline=False)
-    embed.add_field(name="理由", value=f"{reason}", inline=False)
-    await ctx.send(embed=embed)
 
 @bot.event
 async def on_message(message: discord.Message):
@@ -120,14 +61,7 @@ async def _check_url(message: discord.Message):
 
     await bot.process_commands(message)
 
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
 
-
-channel_sent = None
 @tasks.loop(
     time=time(
         hour=4, minute=1,
@@ -165,21 +99,6 @@ async def send_message_every_10sec():
 
 @bot.event
 async def on_ready():
-    guild = bot.guilds[0]
-    norolemember = [i for i in guild.members]  # 全てのメンバー
-    norolemember2 = [i for i in guild.members if len(i.roles) == 1]  # 役職が一つ（everyoneのみ）のメンバー
-    print("on_ready")
-    print(discord.__version__)
-    print(norolemember)
-    for item in norolemember:
-        print(item)
-
-    print("on_ready")
-    print(discord.__version__)
-    print(norolemember2)
-    for item in norolemember2:
-        print(item)
-
     global channel_sent
     channel_sent = bot.get_channel(1012237139729199136)
     send_message_every_10sec.start()  # 定期実行するメソッドの後ろに.start()をつける
